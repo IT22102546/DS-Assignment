4878f2da9056f33e6d369fc54bdb4b93a176809b
adwx2001 <asankadilshanme@gmail.com>
2025-04-20 12:25:01 +0530

    feat: order service is updated

---
+++ OrderService/index.js
@@ -0,0 +1,47 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+//routes import
+import orderRoute from "./routes/order.route.js";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(4000, () => {
+  console.log("Server is Running on Port 4000");
+});
+
+const corsOptions = {
+  origin: " http://localhost:5173/",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+//api call
+app.use("/api/order", orderRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
